9f7c5dc6718ffdab34679b8c8ac08c2de8ea9698
nadyrbek97 <nadyrbek97@gmail.com>
2019-07-30 15:02:39 +0600

    add Home form and view

--- converter/views.py
+++ converter/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import YoutubeFileForm
 
-# Create your views here.
+
+def home_view(request):
+
+    if request.method == "POST":
+        form = YoutubeFileForm(request.POST)
+        if form.is_valid():
+            link = form.cleaned_data.get('link')
+            email = form.cleaned_data.get('email')
+            return render(request, 'converter/home.html', {'form': form})
+    form = YoutubeFileForm(request.GET)
+    return render(request, 'converter/home.html', {'form': form})
