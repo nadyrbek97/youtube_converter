b2b275e5f9a0b729955bfc51c29b2b4d108167bb
nadyrbek97 <nadyrbek97@gmail.com>
2019-08-05 14:08:34 +0600

    add test

--- converter/tests.py
+++ converter/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+
+    def setUp(self):
+        self.test_link = "https://www.youtube.com/watch?v=Mhj15W23IjA"
+        self.test_email = "nadyrbek97@gmail.com"
+        self.client = Client()
+        self.home_url = reverse('home-view')
+
+    def test_post_view(self):
+
+        response = self.client.post(self.home_url, {'link': self.test_link,
+                                                    'email': self.test_email})
+
+        self.assertEqual(response.status_code, 200)
